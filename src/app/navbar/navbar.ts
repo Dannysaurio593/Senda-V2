e9b44e0f8e80fe7b20b6c6dc217c3c180520499c
Danny Santacruz <dannyecuador@hotmail.com>
2026-05-19 17:31:18 -0500

    Se corrige el funcionamiento del menú Dropdown

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  isDropdownOpen = false;
+  private closeTimeout: any;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown() {
+    this.isDropdownOpen = false;
+  }
+
+  closeDropdownWithDelay() {
+    if (this.closeTimeout) {
+      clearTimeout(this.closeTimeout);
+    }
+    this.closeTimeout = setTimeout(() => {
+      this.closeDropdown();
+    }, 200);
+  }
+}
